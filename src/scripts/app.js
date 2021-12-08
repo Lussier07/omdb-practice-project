4b91151379a39da97ad8a012678f373ff9c98c07
Brendan <brendan.lussier07@gmail.com>
2021-12-08 02:57:52 -0600

    Implement fetch request

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,13 @@
 // http://www.omdbapi.com/?apikey=[yourkey]&
 
+const getMovies = () => {
+  return fetch('http://www.omdbapi.com/?apikey=3cb6f4da&t=Star+wars&r').then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+  }).catch("failed")
+}
+
+getMovies().then(response => {
+  console.log(response);
+})
